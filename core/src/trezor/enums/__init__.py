e7b6561697c6ffdc3dc2b0fbe0b22391096b1568
Pavol Rusnak <pavol@rusnak.io>
2021-07-15 22:50:26 +0200

    feat(common): add support for Taproot input and output scripts

--- core/src/trezor/enums/__init__.py
+++ core/src/trezor/enums/__init__.py
@@ -279,6 +279,7 @@ if TYPE_CHECKING:
         EXTERNAL = 2
         SPENDWITNESS = 3
         SPENDP2SHWITNESS = 4
+        SPENDTAPROOT = 5
 
     class OutputScriptType(IntEnum):
         PAYTOADDRESS = 0
@@ -287,6 +288,7 @@ if TYPE_CHECKING:
         PAYTOOPRETURN = 3
         PAYTOWITNESS = 4
         PAYTOP2SHWITNESS = 5
+        PAYTOTAPROOT = 6
 
     class DecredStakingSpendType(IntEnum):
         SSGen = 0
